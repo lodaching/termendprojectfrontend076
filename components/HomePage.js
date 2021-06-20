d9a5f60008e72f453532a3a43be29fb5721d100f
lodaching <lodachinglo@gmail.com>
2021-06-21 00:01:30 +0800

    reference theme

--- components/HomePage.js
+++ components/HomePage.js
@@ -19,7 +19,6 @@ export default function HomePage({ datas }) {
             <HomePageItem key={id} title={Title} image={url} description={description} href={'introduce/' + stages[Title]}/>
           ))}
         </div>
-        {/* <HomePageItem title='title' description='description' image='https://res.cloudinary.com/dvedvf7xw/image/upload/v1624177546/large_12027265_177418039263478_3993026811607921277_o_8ba5d0c651.jpg'/> */}
 
       </div>
       
