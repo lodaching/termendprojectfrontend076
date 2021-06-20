e0a6152e3a4840ed8acb9bba24fb6985a1934066
lodaching <lodachinglo@gmail.com>
2021-06-20 21:53:34 +0800

    introduce page

---
+++ pages/introduce/senior.js
@@ -0,0 +1,18 @@
+import IntroducePage from '../../components/IntroducePage'
+
+export async function getServerSideProps() {
+  const response = await fetch('https://termendproject076.herokuapp.com/Introduces')
+  const datas = await response.json()
+
+  return {
+    props: {
+      datas
+    }
+  }
+}
+
+export default function elementary({datas}) {
+  return (
+    <IntroducePage datas={datas} title={'高中'}/>
+  )
+}
